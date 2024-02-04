9c94a3612b320350e590f1e06376ca06ad043dc6
The Chief <lurbevil@myuax.com>
2024-02-04 18:09:25 +0100

    añadido el modelo stream con el que trabajan los servidores de openai, usare chunks para cargar de forma dinamica la respuesta

--- chat.py
+++ chat.py
@@ -1,12 +1,19 @@
 import openai
 
+openai.api_key= "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"
+
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages= [
         {"role": "system", "content": "Eres un asistente util."},
         {"role": "user", "content": "¿Que puedo hacer por ti?"}
     ],
-    max_tokens=150
+    max_tokens= 350
+    stram=True
 )
 
-print(response)
+collected_messages = []
+
+
+for chunk in response:
+    chunk_message = chunk['choices'][0]['delta']

cf1abb099fa6844583b39ad06b4e694f7e6b4422
The Chief <lurbevil@myuax.com>
2024-02-05 17:28:45 +0100

    Ya funcionan las respuestas al chat

--- ChatBot/main.py
+++ ChatBot/main.py
@@ -9,33 +9,27 @@ client = OpenAI(
 chat_history = []
 
 while True: 
-    prompt = input("Hola, ¿Qué necesitas? \n")
+    prompt = input("\nHola, ¿Qué necesitas? \n")
     
     if prompt == "exit":
         break
         
     else:
         
-        chat_history.append({"role" : "user",  "message" : prompt})
+        chat_history.append({"role" : "user",  "content" : prompt})
         
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages= chat_history,
-            max_tokens= 4000,
+            max_tokens= 100,
             stream=True
         )
 
         collected_messages = []
 
         for chunk in response:
-            chunk_message = chunk['choices'][0]['delta']
-            collected_messages.append(chunk_message)
-            fully_reply_content = ''.join([m.get('content', '') for m in collected_messages]) #Extraigo cada uno de los mensajes de los chunks y los uno para que sean una sola "frase"
-            print(fully_reply_content)
-            print("\033[H\033[J", end="") #Limpio los chunks anteriores de la pantalla para que solo se muestre el ultimo con la frase completa
-            
-        chat_history.append({"role": "assistant", "content" : fully_reply_content})
-        print(fully_reply_content)
+            if chunk.choices[0].delta.content is not None:
+                 print(chunk.choices[0].delta.content, end="")
 
 
 

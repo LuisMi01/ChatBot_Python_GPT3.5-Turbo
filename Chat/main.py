c4aea96343976b6008106e40ca1a1330de5eb90c
The Chief <lurbevil@myuax.com>
2024-02-09 09:55:38 +0100

    reinstalada la nueva version de python y de reflex

---
+++ Chat/main.py
@@ -0,0 +1,36 @@
+import os
+import config
+from openai import OpenAI
+
+client = OpenAI(
+    api_key=config.OPENAI_API_KEY
+    )
+
+chat_history = []
+
+while True: 
+    prompt = input("\nHola, ¿Qué necesitas? \n")
+    
+    if prompt == "exit":
+        break
+        
+    else:
+        
+        chat_history.append({"role" : "user",  "content" : prompt})
+        
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages= chat_history,
+            max_tokens= 1000,
+            stream=True, 
+            temperature=0.8
+        )
+
+        collected_messages = []
+
+        for chunk in response:
+            if chunk.choices[0].delta.content is not None:
+                 print(chunk.choices[0].delta.content, end="")
+
+
+
